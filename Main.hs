22d0bb777abf5aef9391f2c24193dd1b4378636c
junp <junpc97@gmail.com>
2023-07-09 15:58:21 +0800

    2023.7.9

--- Main.hs
+++ Main.hs
@@ -12,6 +12,10 @@ import Control.Monad.Trans
 import Control.Monad.Trans.Writer
 import Control.Monad.Trans.Accum (add)
 import GHC.RTS.Flags (MiscFlags(installSEHHandlers))
+import Control.Applicative (Applicative(liftA2))
+import System.Random
+import Data.Tuple
+import Control.Monad.Trans.Maybe
 
 listDir :: FilePath -> IO [String]
 listDir = fmap (filter notDots) . getDirectoryContents
@@ -39,7 +43,7 @@ newtype MyReader r a = MyReader { runMyReader :: r -> a }
 
 instance Functor (MyReader r) where
     fmap :: (a -> b) -> MyReader r a -> MyReader r b
-    fmap f (MyReader ra) = MyReader $ f . ra
+    fmap f ra = MyReader $ f . runMyReader ra
 
 instance Applicative (MyReader r) where
     pure :: a -> MyReader r a
@@ -106,6 +110,31 @@ readDog = do
     addr <- readDogsAddress
     return $ Dog name addr
 
+getDogsName :: ReaderT Person Maybe DogName
+getDogsName = ReaderT $ \p ->
+    let name = dogName p in
+    if name == DogName "FOO" then Nothing else Just name
+
+getDogsAddress :: ReaderT Person Maybe Address
+getDogsAddress = ReaderT $ \p ->
+    let (Address addr) = address p in
+    if length addr > 10 then Nothing else Just $ Address addr
+
+getDog :: ReaderT Person Maybe Dog
+getDog = do
+    name <- getDogsName
+    addr <- getDogsAddress
+    return $ Dog name addr
+
+person1 :: Person
+person1 = Person (HumanName "aa") (DogName "jj") (Address "pp")
+
+person2 :: Person
+person2 = Person (HumanName "aa") (DogName "FOO") (Address "pp")
+
+person3 :: Person
+person3 = Person (HumanName "aa") (DogName "jj") (Address "pppppppppppppppppppppppppppppppp")
+
 newtype MyReaderT r m a = MyReaderT { runMyReaderT :: r -> m a }
 
 instance Functor m => Functor (MyReaderT r m) where
@@ -127,6 +156,10 @@ instance Monad m => Monad (MyReaderT r m) where
     (MyReaderT rma) >>= f = MyReaderT $ \r -> do
         a <- rma r
         runMyReaderT (f a) r
+
+instance MonadTrans (MyReaderT r) where
+    lift :: m a -> MyReaderT r m a
+    lift a = MyReaderT (const a)
 
 newtype PWriter a = PWriter (a, [String]) deriving (Eq, Show)
 
@@ -169,8 +202,65 @@ padd i j = do
 -- padd' i j = gen i >>=
 --     \i' -> gen j >>= \j' -> PWriter (i' + j', ["add " ++ show i' ++ " " ++ show j'])
 
-
 -- readDog' :: MyReaderT Person Maybe Dog
+
+newtype PState s a = PState { runPState :: s -> (a, s) }
+
+instance Functor (PState s) where
+    fmap :: (a -> b) -> PState s a -> PState s b
+    fmap f sa = PState $ swap . fmap f . swap . runPState sa
+
+instance Applicative (PState s) where
+    pure :: a -> PState s a
+    pure = PState . (,)
+
+    (<*>) :: PState s (a -> b) -> PState s a -> PState s b
+    sf <*> sa = PState $ \s ->
+        let (ab, s') = runPState sf s
+        in runPState (fmap ab sa) s'
+
+join :: PState s (PState s a) -> PState s a
+join ssa = PState $ uncurry runPState . runPState ssa
+
+instance Monad (PState s) where
+    return :: a -> PState s a
+    return = pure
+
+    (>>=) :: PState s a -> (a -> PState s b) -> PState s b
+    sa >>= f = f =<< sa
+
+repM :: Applicative f => Int -> f a -> f [a]
+repM n a = if n <= 0 then pure [] else (:) <$> a <*> repM (n-1) a
+
+m :: PState StdGen Int
+m = PState $ randomR (1, 6)
+
+newtype EitherT e m a = EitherT { runEitherT :: m (Either e a) }
+
+instance Functor m => Functor (EitherT e m) where
+    fmap :: (a -> b) -> EitherT e m a -> EitherT e m b
+    fmap f (EitherT run) = EitherT $ (fmap . fmap) f run
+
+instance Applicative m => Applicative (EitherT e m) where
+    pure :: a -> EitherT e m a
+    pure a = EitherT $ pure (pure a)
+
+    (<*>) :: EitherT e m (a -> b) -> EitherT e m a -> EitherT e m b
+    (EitherT f) <*> (EitherT e) = EitherT $ (<*>) <$> f <*> e
+
+instance Monad m => Monad (EitherT e m) where
+    return :: a -> EitherT e m a
+    return = pure
+
+    (>>=) :: EitherT e m a -> (a -> EitherT e m b) -> EitherT e m b
+    (EitherT ema) >>= f = EitherT $ do
+        ea <- ema
+        case ea of
+            (Left l)  -> return $ Left l
+            (Right r) -> runEitherT $ f r
+
+foo :: MaybeT [] Int
+foo = undefined
 
 main :: IO ()
 main = putStrLn "somehaskell"
